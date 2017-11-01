9885de4ad182f8d230d2a7c7f4edb5e20761b442
Maximilian Rukavina <rukavinm@edu.htl-villach.at>
2017-11-01 21:27:19 +0100

    v1.2

--- chat.py
+++ chat.py
@@ -4,41 +4,52 @@ import threading
 
 
 # ------------------------------------------ SERVER CLASS ----------------------------------------------
+
+
 class ChatServer:
     # declare variables
-    CONNECTIONS = []
-    NICKNAMES = {}  # key is addr(socket duplet) & value is username
+    CONNECTIONS = {}  # key is socket obj & value is username
     SERVER_SOCKET = socket.socket()
     RECV_BUFFER = 4096
     PORT = 0
 
+    KICK_LIST = []  # temp list for users gotten kicked
+    BAN_LIST = []  # self explanatory
+
     # -- constructor
-    def __init__(self, port):
+    def __init__(self, port, banner):
+        # welcome banner
+        print(banner)
+
         self.PORT = port
         # init server_socket
         self.SERVER_SOCKET.bind(('0.0.0.0', self.PORT))
-        self.CONNECTIONS.append(self.SERVER_SOCKET)  # add SERVER_SOCKET to list of active connections
+        self.CONNECTIONS[self.SERVER_SOCKET] = 'SERVER'  # add SERVER_SOCKET to list of active connections
         # log: initiated server
         print('Server initiated on port: ', str(self.PORT))
 
     # -- method to handle connections
-    def handler(self, conn, addr):
+    def handler(self, conn, username):  # username in case of /kick (user removed from list -> error in broadcast)
         while True:
             try:
                 data = conn.recv(self.RECV_BUFFER)
-                self.broadcast(conn, '[' + self.NICKNAMES[addr] + '] ' + data.decode('utf-8'))
+                self.broadcast(conn, '[' + self.CONNECTIONS[conn] + '] ' + data.decode('utf-8'))
                 if not data:
                     break
             except:
                 # host disconnected
                 # remove socket from CONNECTIONS
                 if conn in self.CONNECTIONS:
-                    self.CONNECTIONS.remove(conn)
-                # inform clients that a client disconnected
-                self.broadcast(self.SERVER_SOCKET, '[SERVER] ' + self.NICKNAMES[addr] + ' disconnected from server')
-                # remove from nicknames
-                if addr in self.NICKNAMES.keys():
-                    del self.NICKNAMES[addr]
+                    del self.CONNECTIONS[conn]
+                # check if only kicked or actually disconnected
+                if username in self.KICK_LIST or username in self.BAN_LIST:
+                    # if kicked remove from tmp list
+                    if username in self.KICK_LIST:
+                        self.KICK_LIST.remove(username)
+                    # not send anything
+                else:
+                    # inform clients that a client disconnected
+                    self.broadcast(self.SERVER_SOCKET, '[SERVER] ' + username + ' disconnected from server')
                 break
 
     # -- input handling method
@@ -47,15 +58,60 @@ class ChatServer:
             # wait for user input
             user_input = input('')
             # check input
-            if user_input == 'exit()':  # if the user wants to close the program
+            if user_input == '/exit':  # command to close the server
                 print('Shutting down server')
                 for conn in self.CONNECTIONS:
                     conn.close()
                 sys.exit()
+            elif '/kick' in user_input:  # command to kick a user
+                command = user_input.split(' ')
+                if len(command) >= 2:
+                    usertokick = command[1]
+                    if usertokick in self.CONNECTIONS.values():  # if user exists
+                        # find out the user's address & remove that connection
+                        for conn, user in self.CONNECTIONS.items():
+                            if user == usertokick:
+                                # kick/close socket
+                                conn.send('You were kicked from the server'.encode('utf-8'))
+                                conn.close()
+                                # add to recently-kicked-users list
+                                self.KICK_LIST.append(usertokick)
+                                # remove from active connections
+                                del self.CONNECTIONS[conn]
+                                break
+                        # inform clients that a client was kicked
+                        self.broadcast(self.SERVER_SOCKET, '[SERVER] ' + usertokick + ' was kicked from server')
+                    else:
+                        print('User: ' + usertokick + ' does not exist!')
+            elif '/ban' in user_input:  # command to ban a user
+                command = user_input.split(' ')
+                if len(command) >= 2:
+                    usertoban = command[1]
+                    if usertoban in self.CONNECTIONS.values():  # if user exists
+                        # check if already banned
+                        if usertoban in self.BAN_LIST:
+                            print('User: ' + usertoban + ' is already banned!')
+                        else:
+                            # find out the user's address & remove that connection
+                            for conn, user in self.CONNECTIONS.items():
+                                if user == usertoban:
+                                    # ban/close socket
+                                    conn.send('You were banned from the server'.encode('utf-8'))
+                                    conn.close()
+                                    # add to banned list
+                                    self.BAN_LIST.append(usertoban)
+                                    # remove from active connections
+                                    del self.CONNECTIONS[conn]
+                                    break
+                            # inform clients that a client was kicked
+                            self.broadcast(self.SERVER_SOCKET, '[SERVER] ' + usertoban + ' was banned from server')
+
+                    else:
+                        print('User: ' + usertoban + ' does not exist!')
 
     # -- method to broadcast to all but the sender
     def broadcast(self, sender, msg):
-        for conn in self.CONNECTIONS:
+        for conn in self.CONNECTIONS.keys():
             if conn != self.SERVER_SOCKET and conn != sender:
                 # if not sender: try to send data
                 conn.send(msg.encode('utf-8'))
@@ -79,23 +135,44 @@ class ChatServer:
             username = conn.recv(1024)  # receive username
             username = username.decode('utf-8')
             taken = False
-            for name in self.NICKNAMES.values():
-                if name == username:  # see if username is already taken
+            # check if username e.g. [SERVER]
+            if username.lower() in '[server]':
+                taken = True
+                conn.send('taken'.encode('utf-8'))  # send back 'taken'
+                conn.close()  # close connection
+            if not taken:
+                # check if contains braces
+                for c in ['(', ')', '[', ']', '{', '}', '/', '\\']:
+                    if c in username:
+                        taken = True
+                        conn.send('taken'.encode('utf-8'))  # send back 'taken'
+                        conn.close()  # close connection
+                        break
+            if not taken:
+                # check if username is already taken
+                for name in self.CONNECTIONS.values():
+                    if name == username:  # see if username is already taken
+                        taken = True
+                        conn.send('taken'.encode('utf-8'))  # send back 'taken'
+                        conn.close()  # close connection
+                        break
+            if not taken:
+                # check if username is banned
+                if username in self.BAN_LIST:
                     taken = True
                     conn.send('taken'.encode('utf-8'))  # send back 'taken'
                     conn.close()  # close connection
-            if not taken:
+            if not taken:  # passed all checks
                 conn.send('not-taken'.encode('utf-8'))  # send back 'not-taken'
-                # add to nickname database
-                self.NICKNAMES[addr] = username
+                # add to nickname database & add to list of active connections
+                self.CONNECTIONS[conn] = username
 
                 # create new handling-thread for connection
                 # it shall target the handler & be a daemon (die if parent dies)
-                c_thread = threading.Thread(target=self.handler, args=(conn, addr), daemon=True)
+                c_thread = threading.Thread(target=self.handler, args=(conn, username), daemon=True)
                 c_thread.start()
-                self.CONNECTIONS.append(conn)  # add the new connection to the list of active connections
                 # inform client of the connect
-                self.broadcast(self.SERVER_SOCKET, '[SERVER] ' + self.NICKNAMES[addr] + ' connected to server')
+                self.broadcast(self.SERVER_SOCKET, '[SERVER] ' + self.CONNECTIONS[conn] + ' connected to server')
 
 
 # ------------------------------------------ CLIENT CLASS ----------------------------------------------------------
@@ -106,7 +183,10 @@ class ChatClient:
     USERNAME = ''
 
     # -- constructor
-    def __init__(self, hostname, port, username):
+    def __init__(self, hostname, port, username, banner):
+        # welcome banner
+        print(banner)
+
         # try to connect to server
         try:
             print('Trying to connect to ' + str(hostname) + ' on port ' + str(port))
@@ -144,10 +224,10 @@ class ChatClient:
     def input_handler(self):
         while True:
             # wait for user input -> send to server
-            user_input = input()
+            user_input = input('[' + self.USERNAME + '] ')
 
             # check input
-            if user_input == 'exit()':  # if the user wants to close the program
+            if user_input == '/exit':  # if the user wants to close the program
                 self.CLIENT_SOCKET.close()
                 sys.exit()
             elif user_input != '':
@@ -159,12 +239,17 @@ class ChatClient:
 
 
 # ---------------------------------------------- ENTRY POINT ----------------------------------------------
+# declare welcome banner
+banner = "_____-----========== Python LAN-Chatter ==========-----_____\n" \
+         "_____===================== V 1.2 ======================_____\n" \
+         "     -----======== by Maximilian Rukavina ========-----     \n"
+
 # given enough arguments?
 if len(sys.argv) > 1:  # more than the standard argument?
     if str(sys.argv[1]) == 'server' and len(sys.argv) == 3:  # server & 3 argument in total?
         # try to run server
         try:
-            server = ChatServer(int(sys.argv[2]))
+            server = ChatServer(int(sys.argv[2]), banner)
             server.run()
         except:
             print('SERVER CRASHED')
@@ -172,7 +257,7 @@ if len(sys.argv) > 1:  # more than the standard argument?
     elif str(sys.argv[1]) == 'client' and len(sys.argv) == 5:  # if client & 4 arguments in total?
         # try to create client
         try:
-            client = ChatClient(str(sys.argv[2]), int(sys.argv[3]), str(sys.argv[4]))
+            client = ChatClient(str(sys.argv[2]), int(sys.argv[3]), str(sys.argv[4]), banner)
         except:
             print('CLIENT CRASHED')
             sys.exit()
